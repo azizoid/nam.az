fcc2b0dfe66bdc2cc9c6751bddcd8652fb4253c9
Azizoid <shaqhuseynov@gmail.com>
2021-11-07 06:13:12 +0100

    footer

--- src/components/footer.component.tsx
+++ src/components/footer.component.tsx
@@ -1,29 +1,14 @@
 import React from "react";
 
 export const Footer = ():JSX.Element => (
-  <div className="footer">
-    <nav>
-      <ol className="breadcrumb justify-content-center">
-        <li className="breadcrumb-item active">&copy; 2021</li>
-        <li className="breadcrumb-item">
-          <a href="https://www.nam.az">Nam.az</a>
-        </li>
-
-        <li className="breadcrumb-item">
-          <a
-            href="https://www.quran.az"
-            rel=" noopener noreferrer"
-            target="_blank"
-          >
-            Quran.az
-          </a>
-        </li>
-        <li className="breadcrumb-item active">
-          <small className="disabled">
-            Namaz Vaxtları: <u>islamicfinder.com</u>
-          </small>
-        </li>
-      </ol>
-    </nav>
+  <nav className="navbar fixed-bottom ">
+  <div className="container">
+  <ol className='breadcrumb justify-content-center'>
+      <li className="breadcrumb-item active" aria-current="page">&copy; {new Date().getFullYear()}</li>
+      <li className="breadcrumb-item"><a href="https://www.nam.az">Nam.az</a></li>
+      <li className="breadcrumb-item"><a href="https://www.quran.az" rel=" noopener noreferrer" target="_blank">Quran.az</a></li>
+    </ol>
   </div>
+</nav>
+  
 );
