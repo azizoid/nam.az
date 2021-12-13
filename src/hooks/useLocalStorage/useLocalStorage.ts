7be3ed0bd8229839ff84e34fc3d238fae621dad2
azizoid <shaqhuseynov@gmail.com>
2021-12-13 01:04:01 +0100

    feat: add useLocalStorage hook

---
+++ src/hooks/useLocalStorage/useLocalStorage.ts
@@ -0,0 +1,19 @@
+import React from 'react';
+
+export const useLocalStorage = <T>(
+  storageName: string,
+  defaultValue: T
+): [T, React.Dispatch<T>] => {
+  const [state, setState] = React.useState<T>(() => {
+    const item = localStorage.getItem(storageName);
+    if (item) {
+      return JSON.parse(item);
+    }
+    return defaultValue;
+  });
+  React.useEffect(() => {
+    localStorage.setItem(storageName, JSON.stringify(state));
+  }, [storageName, state]);
+
+  return [state, setState];
+};
