c3a0f734b95606906a50e141163c88369cb693cd
azizoid <shaqhuseynov@gmail.com>
2022-01-28 03:32:24 +0100

    update random ayah

--- src/components/Ayah/Ayah.tsx
+++ src/components/Ayah/Ayah.tsx
@@ -9,28 +9,39 @@ const fetchAyah = async () => {
   return response.json();
 };
 
+export type AyahApiProps = {
+  id: string;
+  soorah: number;
+  ayah: number;
+  content: string;
+  content_latinized: string;
+  translator: number;
+};
+
 const Ayah = (): JSX.Element => {
-  const [ayah, setAyah] = useState({
-    content: {
-      s: 40,
-      a: 60,
-      c: "Rəbbiniz dedi: 'Mənə dua edin, Mən də sizə cavab verim. Həqiqətən, Mənə ibadət etməyə təkəbbür göstərənlər Cəhənnəmə zəlil olaraq girəcəklər'.",
-    },
+  const [ayah, setAyah] = useState<AyahApiProps>({
+    id: 'defaultID',
+    soorah: 40,
+    ayah: 60,
+    content:
+      "Rəbbiniz dedi: 'Mənə dua edin, Mən də sizə cavab verim. Həqiqətən, Mənə ibadət etməyə təkəbbür göstərənlər Cəhənnəmə zəlil olaraq girəcəklər'.",
+    content_latinized: '',
+    translator: 4,
   });
 
   useEffect(() => {
-    fetchAyah().then(data => setAyah({ content: data.out }));
+    fetchAyah().then(({ out }) => setAyah(out));
   }, []);
 
   return (
     <blockquote className={styles.ayah}>
       <MdFormatQuote style={{ color: '#66cc66' }} />
       <cite>
-        {ayah.content.s} : {ayah.content.a}
+        {ayah.soorah} : {ayah.ayah}
       </cite>
-      {ayah.content.c}
+      {ayah.content}
       <a
-        href={`https://quran.az/${ayah.content.s}#${ayah.content.a}?rel=namaz`}
+        href={`https://quran.az/${ayah.soorah}#${ayah.ayah}?rel=namaz`}
         target="_blank"
         rel="noopener noreferrer"
         className={styles.link}
