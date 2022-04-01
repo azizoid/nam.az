44ec88da642fdf91b966779bd0492eab105af9e6
azizoid <shaqhuseynov@gmail.com>
2022-04-02 01:05:09 +0200

    add hijri monthnames

---
+++ src/assist/hijriMonthList.ts
@@ -0,0 +1,14 @@
+export const hijriMonthList: string[] = [
+  'Məhərrəm',
+  'Səfər',
+  'Rəbbiüləvvəl',
+  'Rəbbiülaxır',
+  'Cəmadiyəəvvəl',
+  'Cəmadiəlaxır',
+  'Rəcəb',
+  'Şaban',
+  'Ramazan',
+  'Şəvval',
+  'Zilqədə',
+  'Zilhiccə',
+];
