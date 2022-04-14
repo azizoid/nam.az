557edb8ea2cc6a272130ff7c17dd220dec282824
azizoid <shaqhuseynov@gmail.com>
2022-04-14 02:02:43 +0200

    add Footer tests

---
+++ src/components/Footer/Footer.test.tsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react';
+import { Footer } from './Footer';
+
+test('renders Footer component', () => {
+  const { container } = render(<Footer />);
+
+  expect(container).toMatchSnapshot();
+});
