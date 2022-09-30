afc97bee6f67202e10242c4cb0d02a1b35eb9764
azizoid <shaqhuseynov@gmail.com>
2022-09-30 13:09:30 +0200

    fix scroll issue on mobile screens

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -6,38 +6,40 @@ export type HeaderProps = {
 };
 
 export const Header = ({ changeCity, city }: HeaderProps): JSX.Element => (
-  <nav className="flex justify-between container mx-auto">
-    <a
-      className="py-2 flex items-center content-start text-gray-500 hover:opacity-75"
-      href="/"
-    >
-      <img
-        src="/favicon.png"
-        width="30"
-        height="30"
-        alt="nam.az"
-        className="mr-2"
-      />
-      Nam.az
-    </a>
-
-    <div className="flex flex-col">
-      <select
-        className="border-green-400 bg-green-50 text-sm rounded-md"
-        aria-label="Haradasınız?"
-        onChange={e => changeCity(Number(e.target.value))}
-        value={city}
+  <div className="bg-gray-100 py-2 px-2">
+    <nav className="flex justify-between container mx-auto">
+      <a
+        className="py-2 flex items-center content-start text-gray-500 hover:opacity-75"
+        href="/"
       >
-        {cities.map((cityTitle, index) => (
-          <option value={index} key={index}>
-            {cityTitle}
-          </option>
-        ))}
-      </select>
+        <img
+          src="/favicon.png"
+          width="30"
+          height="30"
+          alt="nam.az"
+          className="mr-2"
+        />
+        Nam.az
+      </a>
 
-      <small>
-        Bakı, Gəncə, <u>Şuşa</u> və digər
-      </small>
-    </div>
-  </nav>
+      <div className="flex flex-col">
+        <select
+          className="border-green-400 bg-green-50 text-sm rounded-md"
+          aria-label="Haradasınız?"
+          onChange={e => changeCity(Number(e.target.value))}
+          value={city}
+        >
+          {cities.map((cityTitle, index) => (
+            <option value={index} key={index}>
+              {cityTitle}
+            </option>
+          ))}
+        </select>
+
+        <small>
+          Bakı, Gəncə, <u>Şuşa</u> və digər
+        </small>
+      </div>
+    </nav>
+  </div>
 );
