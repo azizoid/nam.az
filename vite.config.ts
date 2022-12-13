8e2358d9818bc4a2e89bebc4a84731b38c2cbaaa
azizoid <shaqhuseynov@gmail.com>
2022-12-13 06:54:18 +0100

    chore: change output directory for vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from "vite"
-import react from "@vitejs/plugin-react-swc"
-import path from "path"
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
     manifest: true,
+    outDir: '../build',
   },
   server: {
     port: 3000,
@@ -13,9 +14,9 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      utility: path.resolve("src/utility"),
-      components: path.resolve("src/components"),
-      assist: path.resolve("src/assist"),
+      utility: path.resolve('src/utility'),
+      components: path.resolve('src/components'),
+      assist: path.resolve('src/assist'),
     },
   },
-})
+});
