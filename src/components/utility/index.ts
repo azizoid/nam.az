b587eea8276a5d123db68f6e6c0b9cfcd8c2ab9f
azizoid <shaqhuseynov@gmail.com>
2023-01-08 23:28:18 +0400

    refactor: move generatePrayers to separate function

--- src/components/utility/index.ts
+++ src/components/utility/index.ts
@@ -3,6 +3,7 @@ export { numberSuffixAz } from './numberSuffixAz/numberSuffixAz';
 export { selectCity } from './selectCity/selectCity';
 export { fetchData } from './fetchData/fetchData';
 export type { ResponseDataProps } from './fetchData/fetchData';
+export { generatePrayers } from './generatePrayers/generatePrayers';
 
 // Hooks
 export { useLocalStorage } from './useLocalStorage/useLocalStorage';
