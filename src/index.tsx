9850501c4e933a466ccb7b543bd1d7db4e390787
azizoid <shaqhuseynov@gmail.com>
2023-01-13 20:31:15 +0100

    feature: add a new context provider

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,12 @@
 import { createRoot } from 'react-dom/client';
+import { MyContextProvider } from './store/MyContextProvider';
 import { App } from './App/App';
 
 import './styles/index.css';
 
 const root = createRoot(document.getElementById('root') as HTMLElement);
-root.render(<App />);
+root.render(
+  <MyContextProvider>
+    <App />
+  </MyContextProvider>
+);
