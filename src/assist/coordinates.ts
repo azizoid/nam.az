f52c7bcd2d56f69f9ff651489924d8a2b3f76c59
azizoid <shaqhuseynov@gmail.com>
2023-03-24 15:51:48 +0100

    feature: add city coordinates

---
+++ src/assist/coordinates.ts
@@ -0,0 +1,42 @@
+export type CoordinatesProps = {
+  id: number;
+  city: string;
+  lat: number;
+  lng: number;
+};
+
+export const coordinates: CoordinatesProps[] = [
+  { id: 1, city: 'Bakı', lat: 40.4093, lng: 49.8671 },
+  { id: 2, city: 'Sumqayıt', lat: 40.5855, lng: 49.6317 },
+  { id: 3, city: 'Gəncə', lat: 40.6879, lng: 46.3723 },
+  { id: 4, city: 'Lənkaran', lat: 38.6862, lng: 48.7913 },
+  { id: 5, city: 'Mingəçevir', lat: 40.7703, lng: 47.0496 },
+  { id: 6, city: 'Naxçıvan', lat: 39.2009, lng: 45.4082 },
+  { id: 7, city: 'Ağdam', lat: 39.9911, lng: 46.9297 },
+  { id: 8, city: 'Astara', lat: 38.4688, lng: 48.8728 },
+  { id: 9, city: 'Beyləqan', lat: 39.7723, lng: 47.6154 },
+  { id: 10, city: 'Cəlilabad', lat: 39.2052, lng: 48.5101 },
+  { id: 11, city: 'Göyçay', lat: 40.6236, lng: 47.7403 },
+  { id: 12, city: 'Qəbələ', lat: 40.9982, lng: 47.87 },
+  { id: 13, city: 'Qazax', lat: 41.0971, lng: 45.3516 },
+  { id: 14, city: 'Quba', lat: 41.3643, lng: 48.5261 },
+  { id: 15, city: 'Qusar', lat: 41.4267, lng: 48.4346 },
+  { id: 16, city: 'Saatlı', lat: 39.9096, lng: 48.3595 },
+  { id: 17, city: 'Sabirabad', lat: 39.9871, lng: 48.4693 },
+  { id: 18, city: 'Şamaxı', lat: 40.6319, lng: 48.6364 },
+  { id: 19, city: 'Şəki', lat: 41.1975, lng: 47.1571 },
+  { id: 20, city: 'Şirvan', lat: 39.9266, lng: 48.9206 },
+  { id: 21, city: 'Xaçmaz', lat: 41.4591, lng: 48.8021 },
+  { id: 22, city: 'Yevlax', lat: 40.6197, lng: 47.15 },
+  { id: 23, city: 'Zaqatala', lat: 41.6031, lng: 46.6383 },
+  { id: 24, city: 'Şuşa', lat: 39.7537, lng: 46.7465 },
+  { id: 25, city: 'Marneuli', lat: 41.4689, lng: 44.7851 },
+  { id: 26, city: 'Ərdəbil', lat: 38.2432, lng: 48.2976 },
+  { id: 27, city: 'Füzuli', lat: 39.5379, lng: 47.3034 },
+  { id: 28, city: 'Cəbrayıl', lat: 39.3987, lng: 47.0245 },
+  { id: 29, city: 'Zəngilan', lat: 39.0857, lng: 46.6525 },
+  { id: 30, city: 'Qubadlı', lat: 39.3453, lng: 46.5789 },
+  { id: 31, city: 'Kəlbəcər', lat: 40.1316, lng: 46.1675 },
+  { id: 32, city: 'Laçın', lat: 39.6383, lng: 46.5461 },
+  { id: 33, city: 'Zəngəzur', lat: 39.4202, lng: 45.8022 },
+];
