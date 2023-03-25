d938a86a2ae6d185da4cb839b5e02e0ee7561e39
azizoid <shaqhuseynov@gmail.com>
2023-03-25 22:08:11 +0100

    feature: export ui components from one index file

---
+++ src/components/index.ts
@@ -0,0 +1,8 @@
+export { Footer } from './Footer/Footer';
+
+export { Header } from './Header/Header';
+export type { HeaderProps } from './Header/Header';
+
+export { Loader } from './Loader/Loader';
+
+
