bb367afeb501ee015e6c98d92ee9c1de3456c243
azizoid <shaqhuseynov@gmail.com>
2023-04-02 05:45:27 +0200

    feature: add Location component

---
+++ src/screens/Namaz/Location/Location.test.tsx
@@ -0,0 +1,26 @@
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import MockDate from 'mockdate';
+
+import { Location } from './Location';
+
+const mockDate = Date.parse('2021-12-06T01:14:36.710Z');
+
+MockDate.set(mockDate);
+
+const mockProps = {
+  location: 'Bakı',
+  tarix: 'Şənbə, 2 aprel 2022',
+  dd: 93,
+  changeDd: jest.fn(),
+};
+
+test('renders Location', async () => {
+  const { container, getByText } = render(<Location {...mockProps} />);
+
+  const city = getByText('Bakı');
+
+  expect(city).toBeInTheDocument();
+
+  expect(container).toMatchSnapshot();
+});
---
+++ src/screens/Namaz/Location/Location.tsx
@@ -0,0 +1,41 @@
+import { MdNavigateBefore, MdNavigateNext } from 'react-icons/md';
+
+import { Clock } from './Clock/Clock';
+
+export type LocationProps = {
+  location: string;
+  tarix: string;
+  hijri?: string;
+  dd: number;
+  changeDd: (dd: number) => void;
+};
+
+export const Location = ({
+  location,
+  tarix,
+  hijri,
+  dd,
+  changeDd,
+}: LocationProps) => (
+  <div className="flex justify-around align-middle">
+    <button className="btn text-blue-300" onClick={() => changeDd(dd - 1)}>
+      <MdNavigateBefore />
+    </button>
+
+    <div className="flex flex-col space-y-2 text-center text-3xl font-semibold">
+      <h2 className="text-slate-300">
+        <Clock />
+      </h2>
+
+      <h2>{location}</h2>
+
+      <small className="text-sm font-normal">
+        {tarix}, <span className="whitespace-nowrap">{hijri}</span>
+      </small>
+    </div>
+
+    <button className="btn text-blue-300" onClick={() => changeDd(dd + 1)}>
+      <MdNavigateNext />
+    </button>
+  </div>
+);
