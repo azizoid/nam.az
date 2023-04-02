a754d20ee64841a013d5f260677127c15f7cfbfe
azizoid <shaqhuseynov@gmail.com>
2023-04-02 05:44:49 +0200

    feat: add a Clock component

---
+++ src/screens/Namaz/Location/Clock/Clock.tsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from 'react';
+
+import styles from './Clock.module.css';
+
+const date2date = () =>
+  new Date().toLocaleTimeString('az', {
+    timeZone: 'Asia/Baku',
+    hour12: false,
+  });
+
+export const Clock = () => {
+  const [date, setDate] = useState(date2date());
+
+  const tick = () => {
+    setDate(date2date());
+  };
+
+  useEffect(() => {
+    const timerID = setInterval(() => tick(), 1000);
+
+    return () => {
+      clearInterval(timerID);
+    };
+  });
+
+  return <p className={styles.clock}>{date}</p>;
+};
