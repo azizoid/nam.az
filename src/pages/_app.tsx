0925c936565450f316924fcf57ba2d75849a0fac
azizoid <shaqhuseynov@gmail.com>
2023-04-03 03:17:36 +0200

    feature: wrap _app into Provider

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,15 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 import { Layout } from '@/components'
+import { Provider } from 'react-redux'
+import { store } from '@/store'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <Provider store={store}>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </Provider>
   )
 }
