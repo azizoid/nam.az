27f380b117aca320026b17355adc9d7563bd4284
azizoid <shaqhuseynov@gmail.com>
2023-04-03 04:06:19 +0200

    feature: we need to calculate time untill the next prayer to show a progressBar

--- src/utilities/index.ts
+++ src/utilities/index.ts
@@ -1,3 +1,4 @@
 export { fetcher } from './fetcher'
 export { readLocalStorage } from './readLocalStorage/readLocalStorage'
 export { selectCity } from './selectCity/selectCity'
+export { percentageCounter } from './percentageCounter/percentageCounter'
---
+++ src/utilities/percentageCounter/percentageCounter.ts
@@ -0,0 +1,36 @@
+import { differenceInSeconds, parse } from 'date-fns';
+
+type PercentageCouterProps = {
+  currentPrayer: number;
+  apiPrayers: string[];
+  nowis: string;
+  newDate: Date;
+};
+
+export const percentageCounter = ({
+  currentPrayer,
+  apiPrayers,
+  nowis,
+  newDate,
+}: PercentageCouterProps): number => {
+  const untillNow = differenceInSeconds(
+    parse(nowis, 'HH:mm', newDate),
+    parse(apiPrayers[currentPrayer], 'HH:mm', newDate)
+  );
+
+  let untillNext;
+
+  if (currentPrayer === 5) {
+    untillNext = differenceInSeconds(
+      parse('23:59', 'HH:mm', newDate),
+      parse(apiPrayers[currentPrayer], 'HH:mm', newDate)
+    );
+  } else {
+    untillNext = differenceInSeconds(
+      parse(apiPrayers[currentPrayer++], 'HH:mm', newDate),
+      parse(apiPrayers[currentPrayer], 'HH:mm', newDate)
+    );
+  }
+  const progress = Math.abs(Math.floor((untillNow * 100) / untillNext));
+  return progress > 100 ? 100 : progress;
+};
