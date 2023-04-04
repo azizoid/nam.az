d664ba6c62ef9de72c3d574d24685df0161739f2
azizoid <shaqhuseynov@gmail.com>
2023-04-04 02:14:04 +0200

    store: add setCity reducer and action to the redux store

--- src/store/namazSlice.ts
+++ src/store/namazSlice.ts
@@ -18,9 +18,12 @@ export const namazSlice = createSlice({
       state.city = action.payload.city;
       state.dayOfYear = action.payload.dayOfYear;
     },
+    setCity: (state, action: PayloadAction<Pick<NamazState, 'city'>>) => {
+      state.city = action.payload.city
+    }
   },
 });
 
-export const { setNamazData } = namazSlice.actions;
+export const { setNamazData, setCity } = namazSlice.actions;
 
 export const selectNamazData = (state: { namaz: NamazState }) => state.namaz;
