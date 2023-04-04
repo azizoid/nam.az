9ec4e080f45c54b583ad6c602d6693b03b33a277
azizoid <shaqhuseynov@gmail.com>
2023-04-04 02:35:55 +0200

    refactor: remove unused setCity redicer

--- src/store/namazSlice.ts
+++ src/store/namazSlice.ts
@@ -17,13 +17,10 @@ export const namazSlice = createSlice({
     setNamazData: (state, action: PayloadAction<NamazState>) => {
       state.city = action.payload.city;
       state.dayOfYear = action.payload.dayOfYear;
-    },
-    setCity: (state, action: PayloadAction<Pick<NamazState, 'city'>>) => {
-      state.city = action.payload.city
     }
   },
 });
 
-export const { setNamazData, setCity } = namazSlice.actions;
+export const { setNamazData } = namazSlice.actions;
 
 export const selectNamazData = (state: { namaz: NamazState }) => state.namaz;
