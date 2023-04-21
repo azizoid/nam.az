246ae69b7076c92b13ccfbda4ed9f59ceb4aa853
azizoid <shaqhuseynov@gmail.com>
2023-04-21 17:57:40 +0200

    fix: next date is +1 instead of +2

--- src/screens/Namaz/Location/Location.tsx
+++ src/screens/Namaz/Location/Location.tsx
@@ -17,35 +17,26 @@ export const Location = ({
   tarix,
   hijri,
   dayOfYear,
-}: LocationProps) => {
-  console.log(dayOfYear)
-  return (
-    <div className="flex justify-around align-middle">
-      <Link href={`/${city}/${dayOfYear - 1}`} className="btn text-blue-300">
-        <MdNavigateBefore />
-      </Link>
-      {/* <button className="btn text-blue-300" onClick={() => navigate(dayOfYear - 1)}>
-        <MdNavigateBefore />
-      </button> */}
+}: LocationProps) => (
+  <div className="flex justify-around align-middle">
+    <Link href={`/${city}/${dayOfYear - 1}`} className="btn text-blue-300">
+      <MdNavigateBefore />
+    </Link>
 
-      <div className="flex flex-col space-y-2 text-center text-3xl font-semibold">
-        <h2 className="text-slate-300">
-          <Clock />
-        </h2>
+    <div className="flex flex-col space-y-2 text-center text-3xl font-semibold">
+      <h2 className="text-slate-300">
+        <Clock />
+      </h2>
 
-        <h2>{location}</h2>
+      <h2>{location}</h2>
 
-        <small className="text-sm font-normal">
-          {tarix}, <span className="whitespace-nowrap">{hijri}</span>
-        </small>
-      </div>
+      <small className="text-sm font-normal">
+        {tarix}, <span className="whitespace-nowrap">{hijri}</span>
+      </small>
+    </div>
 
-      {/* <button className="btn text-blue-300" onClick={() => navigate(dayOfYear + 1)}>
-        <MdNavigateNext />
-      </button> */}
-      <Link href={`/${city}/${dayOfYear + 2}`} className="btn text-blue-300">
-        <MdNavigateNext />
-      </Link>
-    </div >
-  )
-};
+    <Link href={`/${city}/${dayOfYear + 1}`} className="btn text-blue-300">
+      <MdNavigateNext />
+    </Link>
+  </div >
+)
