3eed3ae8f288ace8d6be99038a5b83ac7d91c955
azizoid <shaqhuseynov@gmail.com>
2023-04-21 15:49:39 +0200

    fix: optimize leapYearOffset

--- src/utilities/leapYearOffset/leapYearOffset.ts
+++ src/utilities/leapYearOffset/leapYearOffset.ts
@@ -1,14 +1,18 @@
 import { isLeapYear } from "date-fns"
 
+let cachedIsLeapYear: boolean
+
 export const leapYearOffset = (
   dayOfTheYear: number,
   year: number = new Date().getFullYear()
 ): number => {
-  if (dayOfTheYear > 59) {
-    if (!isLeapYear(new Date(`${year}-04-12`))) {
-      return dayOfTheYear + 1
-    }
+  if (dayOfTheYear <= 59) {
+    return 0
   }
 
-  return dayOfTheYear
+  if (cachedIsLeapYear === undefined) {
+    cachedIsLeapYear = isLeapYear(new Date(`${year}-04-12`))
+  }
+
+  return cachedIsLeapYear ? 0 : 1
 }
