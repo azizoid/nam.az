8552e36a8972525d13365156c0ffa275ef87b01d
azizoid <shaqhuseynov@gmail.com>
2023-04-22 22:53:18 +0200

    feat: finalize with the SEO tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,8 +2,13 @@ import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
-    <Html lang="en">
+    <Html lang="az">
       <Head>
+        <meta charSet="utf-8" />
+        <link rel="icon" href="./favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="theme-color" content="#155724" />
+
         <meta name="description" content="Azərbaycan şəhərləri üzrə namaz vaxtları" />
         <meta name="keywords" content="Azərbaycan, şəhərləri, vaxtları, namaz, namaz vaxt, namaz vaxtları, namaz vaxtları namaz, namaz vaxtları namazları, namaz vaxtları namazları, namaz vaxtları namazları, təqvim" />
 
@@ -18,6 +23,10 @@ export default function Document() {
         <meta name="twitter:title" content="Nam.az - Namazını qıl" />
         <meta name="twitter:description" content="Azərbaycan və azərbaycanlıların yaşadığı şəhərləri üzrə namaz vaxtları" />
         <meta name="twitter:image" content="https://nam.az/img/ogimage.png" />
+
+        <link rel="apple-touch-icon" href="./favicon.png" />
+
+        <link rel="manifest" href="./manifest.json" />
       </Head>
       <body>
         <Main />
