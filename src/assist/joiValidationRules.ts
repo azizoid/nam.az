d32906c14622f33144c63f182fce8840d639afde
azizoid <shaqhuseynov@gmail.com>
2023-04-22 04:57:06 +0200

    fix: coordinates length

--- src/assist/joiValidationRules.ts
+++ src/assist/joiValidationRules.ts
@@ -1,5 +1,5 @@
 import Joi from "joi";
 import { coordinates } from "./coordinates";
 
-export const cityRule = Joi.number().integer().min(0).max(coordinates.length - 1).required()
+export const cityRule = Joi.number().integer().min(1).max(coordinates.length).required()
 export const dayOfYearRule = Joi.number().integer().min(1).max(366).required()
