c67cea72d3cddcf138d04fd467a94564cbda5d37
azizoid <shaqhuseynov@gmail.com>
2023-04-22 22:17:09 +0200

    feature: add GTM to the _app page, but i have feeling it was supposed to be in the _document page

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,11 +1,21 @@
 import '@/styles/globals.css'
+
+import { useEffect } from 'react'
 import type { AppProps } from 'next/app'
+import TagManager from 'react-gtm-module'
+import { Provider } from 'react-redux'
 
 import { Layout } from '@/components'
-import { Provider } from 'react-redux'
 import { store } from '@/store'
 
 export default function App({ Component, pageProps }: AppProps) {
+
+  useEffect(() => {
+    TagManager.initialize({
+      gtmId: process.env.NEXT_PUBLIC_GTM_ID as string
+    })
+  }, [])
+
   return (
     <Provider store={store}>
       <Layout>
