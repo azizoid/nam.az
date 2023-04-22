e3abada027eb3ed684c403f8d99cc75c1a593455
azizoid <shaqhuseynov@gmail.com>
2023-04-22 22:55:40 +0200

    chore: update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  poweredByHeader: false,
+  devIndicators: {
+    buildActivity: false,
+  },
+  swcMinify: true,
+  compress: true,
 }
 
 module.exports = nextConfig
