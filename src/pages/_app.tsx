018559d851195883680ad21824e8599ccafa2186
azizoid <shaqhuseynov@gmail.com>
2023-04-24 03:48:47 +0200

    feat: add google-tag-manager from the nextjs with-gtm repo

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,29 +4,48 @@ import { useEffect } from 'react'
 
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
+import router from 'next/router'
+import Script from 'next/script'
 
-import TagManager from 'react-gtm-module'
 import { Provider } from 'react-redux'
 
 import { Layout } from '@/components'
 import { store } from '@/store'
+import { GTM_ID, pageview } from '@/utilities/gtm'
 
 export default function App({ Component, pageProps }: AppProps) {
 
   useEffect(() => {
-    TagManager.initialize({
-      gtmId: process.env.NEXT_PUBLIC_GTM_ID as string
-    })
-  }, [])
+    router.events.on('routeChangeComplete', pageview)
+    return () => {
+      router.events.off('routeChangeComplete', pageview)
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.events])
 
   return (
-    <Provider store={store}>
-      <Head>
-        <title>Nam.az - Namazını qıl</title><meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </Provider>
+    <>
+      <Script
+        id="gtag-base"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer', '${GTM_ID}');
+          `,
+        }}
+      />
+      <Provider store={store}>
+        <Head>
+          <title>Nam.az - Namazını qıl</title><meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </Provider>
+    </>
   )
 }
--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,5 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document'
 
+import { GTM_ID } from '@/utilities/gtm'
+
 export default function Document() {
   return (
     <Html lang="az">
@@ -26,6 +28,14 @@ export default function Document() {
         <link rel="apple-touch-icon" href="./favicon.png" />
       </Head>
       <body>
+        <noscript>
+          <iframe
+            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
         <Main />
         <NextScript />
       </body>
