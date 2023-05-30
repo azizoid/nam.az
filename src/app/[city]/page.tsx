e6bfeb9668b8ab71e941dac0175e67871500ab48
azizoid <shaqhuseynov@gmail.com>
2023-05-30 23:16:47 +0200

    refactor: move validation param check to the server rendered component, handle fetch inside the view

--- src/app/[city]/page.tsx
+++ src/app/[city]/page.tsx
@@ -1,23 +1,16 @@
-'use client'
-import dynamic from 'next/dynamic'
-import Error from 'next/error'
-
 import Joi from 'joi'
-import useSWR from 'swr'
 
 import { cityRule } from '@/assist/joiValidationRules'
-import { CutLoader } from '@/components/CutLoader/CutLoader'
-import { Loader } from '@/components/Loader/Loader'
-import { fetcher } from '@/utilities/fetcher'
 
-const Namaz = dynamic(() => import('@/screens/Namaz/Namaz').then(page => page.Namaz), { ssr: false })
-const Ayah = dynamic(() => import('@/components/Ayah/Ayah').then(page => page.Ayah), { ssr: false, loading: () => <CutLoader /> })
+import ErrorPage from '../error'
+
+import { CityPageView } from './pageView'
+
+export type CityPageProps = { params: { city: string } }
 
 const schema = Joi.object({
   city: cityRule,
 })
-
-export type CityPageProps = { params: { city: string } }
 
 const CityPage = ({ params }: CityPageProps) => {
   const { city = null } = params
@@ -25,26 +18,11 @@ const CityPage = ({ params }: CityPageProps) => {
   // Validate city query using Joi
   const { value, error } = schema.validate({ city: Number(city) })
 
-  const { data, error: fetchError } = useSWR(value.city && !error ? `/api/v2/${value.city}` : null, fetcher, {
-    revalidateOnMount: true,
-    dedupingInterval: 60 * 60 * 1000, // TTL of 1 hour
-  })
-
-  if (city && error) {
-    return <Error statusCode={404} /> // Render the built-in 404 (Not Found) page
+  if (error) {
+    return <ErrorPage />
   }
 
-  if (fetchError) {
-    return <Error statusCode={400} /> // Render the built-in 400 (Bad Request) page
-  }
-  if (!data) {
-    return <Loader />
-  }
-
-  return <>
-    <Namaz data={data} />
-    <Ayah />
-  </>
+  return <CityPageView city={value.city} />
 }
 
 export default CityPage
