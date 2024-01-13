5dc472a600387f855c3fdbc01713219ec263b342
azizoid <shaqhuseynov@gmail.com>
2024-01-14 00:05:33 +0100

    update test files

--- src/components/Footer/Footer.test.tsx
+++ src/components/Footer/Footer.test.tsx
@@ -1,6 +1,12 @@
 import { render } from '@testing-library/react'
 
+import MockDate from 'mockdate'
+
 import { Footer } from './Footer'
+
+const mockDate = Date.parse('2023-12-06T01:14:36.710Z')
+
+MockDate.set(mockDate)
 
 test('renders Footer component', () => {
   const { container } = render(<Footer />)
