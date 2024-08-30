285b680320295d795fda861702e5c8dc82ea788c
azizoid <shaqhuseynov@gmail.com>
2024-08-31 01:26:37 +0200

    log: make logs understandable

--- src/utilities/dayOfYearToDate.ts
+++ src/utilities/dayOfYearToDate.ts
@@ -2,7 +2,7 @@ import { isLeapYear } from 'date-fns'
 
 export function dayOfYearToDate(dayOfYear: number, year: number): Date {
   if (dayOfYear < 1 || dayOfYear > 366 || (dayOfYear > 365 && !isLeapYear(year))) {
-    throw new Error('Invalid day of the year')
+    throw new Error(`Invalid day of the year: ${dayOfYear}`)
   }
 
   return new Date(year, 0, dayOfYear, 12)
