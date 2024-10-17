340a408559d2d31b4e5878487d1aaa44ea2e38f3
azizoid <shaqhuseynov@gmail.com>
2024-10-17 15:14:01 +0200

    feat: use date-fns tool for timezone offset

--- src/utilities/getUtcOffset.ts
+++ src/utilities/getUtcOffset.ts
@@ -1,10 +1,10 @@
-import { getTimezoneOffset } from 'date-fns-tz'
+import { tzOffset } from '@date-fns/tz'
 import tzLookup from 'tz-lookup'
 
 export function getUtcOffset(latitude: number, longitude: number): number {
   const timezone = tzLookup(latitude, longitude)
   const now = new Date()
-  const offset = getTimezoneOffset(timezone, now) / 60000
+  const offset = tzOffset(timezone, now)
 
   return offset
 }
