c0c0dfda97f7464c3b2190ebdbe557b7e5ab8978
azizoid <shaqhuseynov@gmail.com>
2024-10-17 15:25:41 +0200

    feat: use `@photostructure/tz-lookup` to get a timezone number

--- src/utilities/getUtcOffset.ts
+++ src/utilities/getUtcOffset.ts
@@ -1,5 +1,5 @@
 import { tzOffset } from '@date-fns/tz'
-import tzLookup from 'tz-lookup'
+import tzLookup from '@photostructure/tz-lookup'
 
 export function getUtcOffset(latitude: number, longitude: number): number {
   const timezone = tzLookup(latitude, longitude)
