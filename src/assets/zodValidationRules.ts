97d9be9d72068262ae4db60e932a063b5229c021
azizoid <shaqhuseynov@gmail.com>
2024-10-22 04:29:19 +0200

    feat: migrate rules to zod

---
+++ src/assets/zodValidationRules.ts
@@ -0,0 +1,13 @@
+import { getDaysInYear } from 'date-fns'
+import { z } from 'zod'
+
+import { coordinates } from './coordinates'
+
+const slugs = coordinates.map(city => city.slug)
+
+console.log({ slugs })
+
+export const cityRule = z.string().refine(value => slugs.includes(value), {
+  message: 'Invalid city slug',
+})
+export const dayOfYearRule = z.number().int().min(1).max(getDaysInYear(new Date()))
